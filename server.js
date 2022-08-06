2628633f7b2633bea287321ce574bdc083672474
GiriPai <giripaiu@outlook.com>
2022-08-06 21:28:01 +0530

    adding boilers

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+
+// Determine the environment
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  dotenv.config({ path: "./config/.env.local" });
+} else {
+  dotenv.config({ path: "./config/.env" });
+}
+
+// Init express
+const app = express();
+
+// Initialising cors
+app.use(cors());
+
+// Initialising helment
+app.use(helmet());
+
+// Body Parser
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hai"));
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+const mainserver = server.listen(
+  PORT,
+  console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit server
+  mainserver.close(() => process.exit(1));
+});
